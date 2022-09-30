01e150276a7400be814a5f025ad69b70f553a1ac
kyrychenkoartem <killalik2009@gmail.com>
2022-09-29 22:10:26 -0700

    Week 2 - Made OOP task.

---
+++ src/com/artem/week2/oop/Apartment.java
@@ -0,0 +1,32 @@
+package com.artem.week2.oop;
+
+public class Apartment {
+
+    private static final String CONSOLE_PRINT_TEMPLATE = "Apartment number: is %d, number of bedrooms: are %d.";
+
+    private int number;
+
+    private Bedroom[] bedrooms;
+
+    public Apartment(int number, Bedroom[] bedrooms) {
+        this.number = number;
+        this.bedrooms = bedrooms;
+    }
+
+    public Bedroom[] getBedrooms() {
+        return bedrooms;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    void print() {
+        System.out.printf(CONSOLE_PRINT_TEMPLATE, getNumber(), getBedrooms().length);
+        System.out.println();
+        for (int i = 0, counter = 1; i < getBedrooms().length; i++, counter++) {
+            getBedrooms()[i].print(counter);
+            System.out.println();
+        }
+    }
+}
